1b757a553b1cb661b536163ed5c53b3501771419
Choi_ASUS <chso1324@naver.com>
2022-06-18 21:20:50 +0900

    React와Spring 연동하기 1. Spring initalizr로 Spring boot 프로젝트 생성   - Gradle project   - Spring boot 2.6.8   - Jar   - java8 2. project main에 react 앱 생성   - npx create-react-app appname 3. React App의 packge.json에 proxy 설정 추가   - proxy : http://localhost:8080, 4. build.gradle에 def frontendDir ... 추가 5. Gradle -> build -> build로 project rebuild

---
+++ src/test/java/com/react/exam/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.react.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
